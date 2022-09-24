10e95161992c6bb89128932b28ebb2ab631d7e1d
Hridoy Banik <hridoy.banik71@gmail.com>
2022-09-24 12:57:01 +0600

    job router and controller added

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,5 @@
 import express from 'express'
 const router = express.Router()
-
 import rateLimiter from 'express-rate-limit'
 const apiLimiter = rateLimiter({
   windowMs: 15 * 60 * 1000, // 15 minutes
