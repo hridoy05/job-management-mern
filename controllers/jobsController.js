06196abfafd9f7a8f9fe311c24c57ddb750a1155
Hridoy Banik <hridoy.banik71@gmail.com>
2022-09-30 16:35:30 +0600

    monthlyApplications server logic added

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -118,13 +118,50 @@ const showStats = async (req, res) => {
     acc[title] = count
     return acc
   },{})
-  console.log(stats);
   const defaultState = {
     pending: stats.pending || 0,
     interview: stats.pending || 0,
     declined: stats.pending || 0
   }
-  let monthlyApplications = []
+  let monthlyApplications = await Job.aggregate([
+    {$match: { createdBy: mongoose.Types.ObjectId(req.user.userId)}},
+    {$group: {
+      _id: {
+        year: {
+          $year: '$createdAt'
+        },
+        month:{
+          $month:'$createdAt'
+        }
+      },
+      count:{
+        $sum: 1
+      }
+    }
+  },
+  {
+    $sort: {
+      '_id.year': -1, '_id.month': -1
+    },
+  },
+  {
+    $limit:6
+  }
+  ])
+  monthlyApplications = monthlyApplications
+  .map((item) => {
+    const {
+      _id: { year, month },
+      count,
+    } = item
+    // accepts 0-11
+    const date = moment()
+      .month(month - 1)
+      .year(year)
+      .format('MMM Y')
+    return { date, count }
+  })
+  .reverse()
   res.status(StatusCodes.OK).json({ defaultState, monthlyApplications });
 };
 
