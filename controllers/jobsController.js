dcf5358911da604d0dd74add7d5313239ee70b63
Hridoy Banik <hridoy.banik71@gmail.com>
2022-10-07 22:51:10 +0600

    pagination server added

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -56,17 +56,18 @@ const getAllJobs = async (req, res) => {
   }
 
   //setup pagination
-  const page = Number(req.query.page) || 1;
-  const limit = Number(req.query.limit) || 10;
-  const skip = (page - 1) * limit;
+  const page = Number(req.query.page) || 1
+  const limit = Number(req.query.limit) || 10
+  const skip = (page - 1) * limit
 
-  result = result.skip(skip).limit(limit);
+  result = result.skip(skip).limit(limit)
 
-  const jobs = await result;
-  const totalJobs = await Job.countDocuments(queryObject);
-  const numOfPages = Math.ceil(totalJobs / limit);
+  const jobs = await result
 
-  res.status(StatusCodes.OK).json({ jobs, totalJobs, numOfPages: 3 });
+  const totalJobs = await Job.countDocuments(queryObject)
+  const numOfPages = Math.ceil(totalJobs / limit)
+
+  res.status(StatusCodes.OK).json({ jobs, totalJobs, numOfPages });
 };
 const updateJob = async (req, res) => {
   const { id: jobId } = req.params;
