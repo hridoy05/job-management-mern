1d20a6b874542f39692810c73124cdaa4a3a0783
hridoy05 <hridoy.banik71@gmail.com>
2024-07-10 11:22:37 +0600

    fix deploy code

--- app.js
+++ app.js
@@ -25,10 +25,10 @@ import path from "path";
 import { handleRequest } from "./middleware/handle-request.js";
 //import { errorLogger, infoLogger } from "./logger/logger.js";
 
-// const __dirname = dirname(fileURLToPath(import.meta.url))
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 // // only when ready to deploy
-// app.use(express.static(path.resolve(__dirname, './client/build')))
+app.use(express.static(path.resolve(__dirname, './client/build')))
 
 app.use(express.json());
 app.use(helmet());
